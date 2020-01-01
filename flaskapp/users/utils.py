ec0686c41b56b30147b2b34bcfcf73b8e633f75a
Bartosz Żelek <bartosz.zelek@poczta.onet.pl>
2020-01-01 22:06:01 +0100

    Repair password recovery

--- flaskapp/users/utils.py
+++ flaskapp/users/utils.py
@@ -4,13 +4,14 @@ from flaskapp import mail
 
 
 def send_reset_email(user):
-    token = user.get_reset_token()
-    message = Message('Reset Hasła', recipients=[user.email])
-    message.body = f'''Aby przypisać do swojego konta nowe hasło kliknij w poniższy link: 
+    if user:
+        token = user.get_reset_token()
+        message = Message('Reset Hasła', recipients=[user.email])
+        message.body = f'''Aby przypisać do swojego konta nowe hasło kliknij w poniższy link: 
 {url_for('users.reset_password_token', token=token, _external=True)}
 
 
 Ważność linku to 30 minut!
 Zignoruj link, jeżeli to nie ty wywołałeś zmainę hasła.
-    '''
-    mail.send(message)
+'''
+        mail.send(message)
