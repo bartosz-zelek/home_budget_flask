cbb1830b61fee6d815bffb6f33c5ba40389cea19
bartox <56220225+bartox7777@users.noreply.github.com>
2020-03-07 13:09:17 +0100

    Add files via upload

---
+++ flaskapp/config.py
@@ -0,0 +1,13 @@
+class Config:
+    SECRET_KEY = ''
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    BABEL_DEFAULT_LOCALE = 'pl'
+    SEND_FILE_MAX_AGE_DEFAULT = 0
+    MAIL_SERVER = ''
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = ''
+
+# example and hidden data
